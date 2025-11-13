3a5648890226c660c9a085cdfe3d57d2f6624750
Anna10PA <puturidzeana0210@gmail.com>
2025-11-13 21:01:08 +0400

    first commit

---
+++ Exam01/src/App.jsx
@@ -0,0 +1,11 @@
+import MainDiv from "./Components/MainDiv"
+
+function App() {
+  return (
+    <div className='bg-[hsl(0,0%,86%)] h-screen w-full flex items-center justify-center'>
+      <MainDiv />
+    </div>
+  )
+}
+
+export default App
---
+++ Exam01/src/Components/MainDiv.jsx
@@ -0,0 +1,105 @@
+import { useState } from 'react'
+
+function MainDiv() {
+    const [day, setDay] = useState('--')
+    const [month, setMonth] = useState('--')
+    const [years, setYears] = useState('--')
+
+    const [isError, setError] = useState('')
+
+    // calculate
+    let calculate = (day, month, year) => {
+        const curentTime = new Date()
+        const curentDay = curentTime.getDate()
+        const curentMonth = curentTime.getMonth() + 1
+        const curentYear = curentTime.getFullYear()
+
+        if (curentYear < year) {
+            setError('invalid')
+        }
+        
+        // calculate year 
+        let resultYear = curentYear - year 
+        let resultMonth = curentMonth - month 
+        let resultDay = curentDay - day 
+
+        if (resultMonth < 0 || resultMonth === 0  && resultDay < 0) {
+            resultYear --
+            resultMonth += 12
+        }
+
+        console.log(resultMonth, resultYear)
+    }
+
+
+    // error check
+    let checkError = (e) => {
+        let userDay = e.target.day.value
+        let userMonth = e.target.month.value
+        let userYear = e.target.year.value
+
+        if (!userDay || !userMonth || !userYear) {
+            setError('Must be full')
+        } else {
+            calculate(userDay, userMonth, userYear)
+        }
+    }
+    return (
+        <form className='bg-white max-w-[800px] w-[90%] min-h-[500px] rounded-[20px] p-[50px] flex flex-col gap-[50px] rounded-br-[30%]' onSubmit={(e) => {
+            e.preventDefault()
+            checkError(e)
+        }}>
+
+            {/* task 001 */}
+            <div className='flex items-center justify-start gap-[30px]'>
+                <div className='flex flex-col items-start gap-[10px]'>
+                    <h1 className={`tracking-[3px] font-[800] font-Poppins ${!isError ? 'text-[hsl(0,1%,44%)]' : 'text-[hsl(0,100%,67%)]'} text-[14px]`}>DAY</h1>
+                    <input type="number" className={`border border-[1px] ${!isError ? 'border-[hsl(0,0%,94%)]' : 'text-[hsl(0,100%,67%)]'} w-[160px] pl-[20px] py-[10px] outline-none rounded-[5px] text-[32px] font-[800]`} name='day' />
+                    <span className={`${isError ? 'block' : 'hidden'} text-[hsl(0,100%,67%)]`}>{isError} day</span>
+                </div>
+                <div className='flex flex-col items-start gap-[10px]'>
+                    <h1 className={`tracking-[3px] font-[800] font-Poppins ${!isError ? 'text-[hsl(0,1%,44%)]' : 'text-[hsl(0,100%,67%)]'} text-[14px]`}>MONTH</h1>
+                    <input type="number" className={`border border-[1px] ${!isError ? 'border-[hsl(0,0%,94%)]' : 'text-[hsl(0,100%,67%)]'} w-[160px] pl-[20px] py-[10px] outline-none rounded-[5px] text-[32px] font-[800]`} name='month' />
+                    <span className={`${isError ? 'block' : 'hidden'} text-[hsl(0,100%,67%)]`}>{isError} month</span>
+                </div>
+                <div className='flex flex-col items-start gap-[10px]'>
+                    <h1 className={`tracking-[3px] font-[800] font-Poppins ${!isError ? 'text-[hsl(0,1%,44%)]' : 'text-[hsl(0,100%,67%)]'} text-[14px]`}>YEAR</h1>
+                    <input type="number" className={`border border-[1px] ${!isError ? 'border-[hsl(0,0%,94%)]' : 'text-[hsl(0,100%,67%)]'} w-[160px] pl-[20px] py-[10px] outline-none rounded-[5px] text-[32px] font-[800]`} name='year' />
+                    <span className={`${isError ? 'block' : 'hidden'} text-[hsl(0,100%,67%)]`}>{isError} year</span>
+                </div>
+            </div>
+
+            {/* task 002 */}
+            <div className='relative flex items-center'>
+                <hr className='border-[hsl(0,0%,94%)] w-full' />
+                <button className='bg-[#854DFD] h-[70px] w-[70px] rounded-[50%] flex items-center justify-center absolute right-0'>
+                    <img src="/icon-arrow.svg" />
+                </button>
+            </div>
+
+            {/* task 003  */}
+            <div className='leading-[1.2]'>
+                <h1 className='text-[62px] font-[900] font-Poppins italic flex items-center gap-[20px]'>
+                    <span className='text-[#854DFD]'>
+                        {years}
+                    </span>
+                    years
+                </h1>
+                <h1 className='text-[62px] font-[900] font-Poppins italic flex items-center gap-[20px]'>
+                    <span className='text-[#854DFD]'>
+                        {month}
+                    </span>
+                    months
+                </h1>
+                <h1 className='text-[62px] font-[900] font-Poppins italic flex items-center gap-[20px]'>
+                    <span className='text-[#854DFD]'>
+                        {day}
+                    </span>
+                    days
+                </h1>
+            </div>
+        </form>
+    )
+}
+
+export default MainDiv
